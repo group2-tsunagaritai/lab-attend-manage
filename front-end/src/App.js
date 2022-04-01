714ca9f16466497ae4f2b73ea62b0a03520d8d26
ma22080 <ma22080@shibaura-it.ac.jp>
2022-04-01 18:48:08 +0900

    サンプルページ作成

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,20 @@
+// 表示するページを決める所。
+// 全ページで表示されるコンポーネントもここに置く
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bulma/css/bulma.css";
+import Ask from "./pages/Ask";
 import Counter from "./pages/Counter";
 import Home from "./pages/Home";
+import Index from "./components/Index";
 function App() {
   return (
     <div className="App">
+      <Index />
       <BrowserRouter>
-        <ui>
-          <li>
-            <a href="/">/</a>
-          </li>
-          <li>
-            <a href="/counter">/counter</a>
-          </li>
-        </ui>
         <Routes>
           <Route index element={<Home />} />
           <Route path="/counter" element={<Counter />} />
+          <Route path="/ask" element={<Ask />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ front-end/src/components/Index.js
@@ -0,0 +1,12 @@
+export default function Index() {
+  const routes = ["/", "/ask", "/counter"];
+  return (
+    <ul>
+      {routes.map((str) => ( // Arrayオブジェクトのmapメソッド https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+        <li key={str}>
+          <a href={str}>{str}</a>
+        </li>
+      ))}
+    </ul>
+  );
+}
