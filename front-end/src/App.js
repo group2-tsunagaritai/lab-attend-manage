49a55dd24458ac9dc230368103ddb24a6e482a5e
ma22080 <sudo3949@gmail.com>
2022-04-03 01:31:18 +0900

    D&D

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,7 @@ import Home from "./pages/Home";
 import Index from "./components/Index";
 import Pikachu from "./pages/Pikachu";
 import Dog from "./pages/Dog";
+import DragNDrop from "./pages/DragNDrop";
 function App() {
   return (
     <div className="App">
@@ -19,6 +20,7 @@ function App() {
           <Route path="/ask" element={<Ask />} />
           <Route path="/pikachu" element={<Pikachu />} />
           <Route path="/dog" element={<Dog />} />
+          <Route path="/dragndrop" element={<DragNDrop />} />
         </Routes>
       </BrowserRouter>
     </div>
