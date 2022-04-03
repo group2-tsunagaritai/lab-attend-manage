1e56f6ebe8cb65a27ea88097bd3a880cf55d995e
ma22080 <ma22080@shibaura-it.ac.jp>
2022-04-03 12:25:33 +0900

    ページ仮作成

---
+++ front-end/src/pages/laboratory/New.js
@@ -0,0 +1,7 @@
+export default function New() {
+  return (
+    <div>
+      <h2>研究室作成</h2>
+    </div>
+  );
+}
---
+++ front-end/src/pages/laboratory/schedule/New.js
@@ -0,0 +1,7 @@
+export default function New() {
+  return (
+    <div>
+      <h2>スケジュール登録</h2>
+    </div>
+  );
+}
