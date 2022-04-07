5dc482c36341af71bbadbf04550bcc7627a99359
fluixyz-unnatural <sudo3949@gmail.com>
2022-04-07 20:55:23 +0900

    メンバー追加削除

--- front-end/src/pages/laboratory/Edit.js
+++ front-end/src/pages/laboratory/Edit.js
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { useUsers } from "../../utils/apihooks";
+
 function UserCard() {
   return (
     <div className="box">
@@ -6,23 +10,50 @@ function UserCard() {
           <p>名前</p>
           <p>id</p>
         </div>
-        <button className="button">
-          削除
-        </button>
+        <button className="button">削除</button>
       </div>
     </div>
   );
 }
 export default function Edit() {
+  const location = useLocation();
+  const lid = location.pathname.split("/")[2];
+  const [newid, setNewId] = useState("");
+  const users = useUsers(lid);
+  console.log(lid)
+  console.log("users", users);
+
   return (
     <div>
       <h2 className="title">研究室情報編集</h2>
       <div className="flex flex-column gap-4">
         <div className="flex gap-4">
-          <input className="input" type="text" placeholder="ID"></input>
-          <button className="is-primary button">追加</button>
+          <input
+            className="input"
+            type="text"
+            placeholder="ID"
+            value={newid}
+            onChange={(e) => setNewId(e.target.value)}
+          ></input>
+          <button
+            className="is-primary button"
+            onClick={() => {
+              const formdata = new FormData();
+              formdata.append("labratory", Number(lid));
+              fetch(`http://localhost:8000/api/users/${newid}/`, {
+                method: "PATCH",
+                body: formdata,
+              }).then(() => {
+                console.log("add");
+              });
+            }}
+          >
+            追加
+          </button>
         </div>
-        <UserCard />
+        {users.map((user, key) => (
+          <UserCard key={key} />
+        ))}
       </div>
     </div>
   );
