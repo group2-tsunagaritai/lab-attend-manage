00953b5c982f2f218eeb2ab39786a49fb7c17862
ma22080 <ma22080@shibaura-it.ac.jp>
2022-04-07 16:29:06 +0900

    可視化用

--- front-end/src/pages/users/Log.js
+++ front-end/src/pages/users/Log.js
@@ -1,7 +1,35 @@
+import dayjs from "dayjs";
+
+function* range(start, end) {
+  for (let i = start; i < end; i++) yield i;
+}
+
+function Panel({ opacity }) {
+  return <div style={{ opacity: opacity }} className="attend-panel"></div>;
+}
+
 export default function Log() {
+  const logs = [{}, {}, {}];
+  const today = dayjs().startOf("week");
+  const millionago = today.subtract(20, "week");
+  console.log(millionago);
+  console.log(today);
   return (
     <div>
       <h2 className="title">出席ログ確認</h2>
+      <div className="box">
+        <div className="flex">
+          {[...range(0, 20)].map((w) => {
+            return (
+              <div className="flex-column">
+                {[...range(0, 7)].map((w, d) => {
+                  return <Panel key={`${w},${d}`} />;
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
